729b0f8233caa5570a728fcc2f757cf9306dddda
Deathmax <death@deathmax.tk>
2013-09-25 14:19:27 +0800

    Added support for automatically dumping reward table names.
    Based on the assumption that the end of the file is the TOC.
    Has problems as there are more tables than are used, tables
    must be manually added.

--- Warframe_RewardTables/Program.cs
+++ Warframe_RewardTables/Program.cs
@@ -13,8 +13,16 @@ namespace Warframe_RewardTables
         private const double CommonTotal = 75.87;
         private const double UncommonTotal = 22.12;
         private const double RareTotal = 2.01;
+
         private static int TotalTables;
         private static int BadTables;
+
+        private static List<string> rewardList = new List<string>();
+        private static Dictionary<int, string> rewardPos = new Dictionary<int, string>();
+        private static List<int> sortedReward = new List<int>();
+        private static int rewardTableIndex = -1;
+
+        private static List<string> dropTableList = new List<string>(); 
 
         static void Main(string[] args)
         {
@@ -39,6 +47,7 @@ namespace Warframe_RewardTables
         {
             Console.WriteLine("Starting parsing extracted file.");
             var file = File.ReadAllLines(filename);
+            var bigfile = string.Join("", file).Replace("\0", "");
             for (int i = 0; i < file.Length; i++)
             {
                 file[i] = file[i].Replace("\0", "").Trim();
@@ -46,6 +55,90 @@ namespace Warframe_RewardTables
             var list = new List<string>();
             var write = "";
             var start = false;
+            Console.WriteLine("Finding reward table types...");
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (file[i].StartsWith("randomizedItems=") && !file[i].Contains("\"\""))
+                {
+                    var parts = file[i].Split('=');
+                    var nameparts = parts[1].Split('/');
+                    var name = nameparts[nameparts.Length - 1];
+                    if (!rewardList.Contains(parts[1])) rewardList.Add(name);
+                }
+                else if (file[i].Contains("/Lotus/Types/DropTables/") && file[i].Length < 2000)
+                {
+                    var parts = file[i].TrimEnd(',').Split('/');
+                    var name = parts[parts.Length - 1];
+                    if (!dropTableList.Contains(name)) dropTableList.Add(name);
+                }
+            }
+            Console.WriteLine("Found {0} used reward table types.", rewardList.Count);
+            //Manual overrides
+            rewardList.Add("NightmareModeRewards");
+            rewardList.Add("OrokinMissionRewards");
+            rewardList.Add("OrokinMissionRewardsB");
+            rewardList.Add("OrokinMissionRewardsC");
+            rewardList.Add("OrokinMobDefenseRewards");
+            rewardList.Add("OrokinMobDefenseRewardsB");
+            rewardList.Add("OrokinMobDefenseRewardsC");
+            rewardList.Add("RaidMissionRewardsA");
+
+            Console.WriteLine("Searching for {0} reward table types.", rewardList.Count);
+            Console.WriteLine("Found {0} item drop table types.", dropTableList.Count);
+
+            foreach (var table in rewardList)
+            {
+                Console.WriteLine("Searching for " + table + "A");
+                var positions = GetPositions(bigfile, table + "A" + "\u0001");
+                Console.WriteLine("Found {0} instances.", positions.Count);
+                if (positions.Count == 1)
+                    rewardPos.Add(positions[0], table);
+                else if (positions.Count == 2)
+                {
+                    rewardPos.Add(positions[0], table + "-1");
+                    rewardPos.Add(positions[1], table + "-2");
+                }
+                if (table.Contains("Orokin"))
+                {
+                    Console.WriteLine("Searching for " + table);
+                    var positions2 = GetPositions(bigfile, table + "\u0001");
+                    Console.WriteLine("Found {0} instances.", positions2.Count);
+                    switch (positions2.Count)
+                    {
+                        case 1:
+                            if (!rewardPos.ContainsKey(positions2[0]))
+                                rewardPos.Add(positions2[0], table + "?");
+                            break;
+                        case 2:
+                            if (!rewardPos.ContainsKey(positions2[0]))
+                                rewardPos.Add(positions2[0], table + "-11");
+                            if (!rewardPos.ContainsKey(positions2[1]))
+                                rewardPos.Add(positions2[1], table + "-12");
+                            break;
+                    }
+                    if (!table.EndsWith("A")) continue;
+                    var table2 = table.Substring(0, table.Length - 1);
+                    Console.WriteLine("Searching for " + table2);
+                    positions2 = GetPositions(bigfile, table2 + "\u0001");
+                    Console.WriteLine("Found {0} instances.", positions2.Count);
+                    switch (positions2.Count)
+                    {
+                        case 1:
+                            if (!rewardPos.ContainsKey(positions2[0]))
+                                rewardPos.Add(positions2[0], table2 + "?");
+                            break;
+                        case 2:
+                            if (!rewardPos.ContainsKey(positions2[0]))
+                                rewardPos.Add(positions2[0], table2 + "-11");
+                            if (!rewardPos.ContainsKey(positions2[1]))
+                                rewardPos.Add(positions2[1], table2 + "-12");
+                            break;
+                    }
+                }
+            }
+            Console.WriteLine("Found {0} available reward table types.", rewardPos.Count);
+            sortedReward = rewardPos.Keys.ToList();
+            sortedReward.Sort();
             for (int i = 0; i < file.Length; i++)
             {
                 if (file[i].Trim().StartsWith("Tier") && file[i].Trim().EndsWith("{"))
@@ -111,8 +204,12 @@ namespace Warframe_RewardTables
 
         static string ParseExt(string[] file)
         {
+            if (file.Length == 0) return "";
             var tier = int.Parse(file[0].Substring(4, 1)) + 1;
-            Console.WriteLine("New table found, parsing. Tier {0}", tier);
+            if (tier == 1)
+                rewardTableIndex++;
+            var tablename = rewardPos[sortedReward[rewardTableIndex]];
+            Console.WriteLine("Found new table. Name: {0} | Reward Tier: {1}", tablename, tier);
             var list = new List<Item>();
             for (int i = 2; i < file.Length; i = i + 5)
             {
@@ -164,7 +261,8 @@ namespace Warframe_RewardTables
             TotalTables++;
             var matherrors = Math.Abs(1 - interval) > 0.1;
             if (matherrors) BadTables++;
-            var write = "Tier " + tier + (matherrors ? " (Chance calculation flawed)" : "") + ",Chance,Interval,Rarity";
+            var write = "Tier " + tier + " - " + tablename
+                + (matherrors ? " (Chance calculation flawed)" : "") + ",Chance,Interval,Rarity";
             write = list.Aggregate(write, (current, item) => current + ("\n" + item.Name + "," + item.Chance + "," + item.Interval + "," + item.Rarity));
             return write;
         }
@@ -228,6 +326,26 @@ namespace Warframe_RewardTables
             var write = "Tier " + tier + (matherrors ? " (Chance calculation flawed)" : "") + ",Chance,Interval,Rarity";
             write = list.Aggregate(write, (current, item) => current + ("\n" + item.Name + "," + item.Chance + "," + item.Interval + "," + item.Rarity));
             return write;
+        }
+        
+        static List<int> GetPositions(string source, string searchString)
+        {
+            List<int> ret = new List<int>();
+            int len = searchString.Length;
+            int start = -len;
+            while (true)
+            {
+                start = source.IndexOf(searchString, start + len, StringComparison.InvariantCultureIgnoreCase);
+                if (start == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    ret.Add(start);
+                }
+            }
+            return ret;
         }
     }
 
