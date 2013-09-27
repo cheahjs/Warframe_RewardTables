4a574fef9a16a52b985ce993067ba18512f85985
Deathmax <death@deathmax.tk>
2013-09-27 16:54:36 +0800

    Start work on a GUI parser for the various chunks of data.

---
+++ Warframe_PackagesParser/frmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Warframe_PackagesParser
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ParseChunks();
+            ParseLabels();
+        }
+
+        private void ParseChunks()
+        {
+            var chunks = File.ReadAllText("chunks.txt");
+        }
+
+        private void ParseLabels()
+        {
+            var labels = File.ReadAllText("labels.txt");
+        }
+    }
+}
